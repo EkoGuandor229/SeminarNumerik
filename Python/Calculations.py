ccf9e00c4545961b0a026d2edafc1f7886d012b8
Eko229 <ekoguandor229@gmail.com>
2020-05-03 12:00:31 +0200

    Update calculations for demo

--- Python/Calculations.py
+++ Python/Calculations.py
@@ -1,15 +1,16 @@
 import numpy as np
 from scipy import integrate
 
+# toleranz = 1.49e-08
+
 func = lambda x: np.sqrt(1 - x ** 2)
-intervals = [2, 3, 4, 5, 10, 100, 1000, 10000]
-
-for interval in intervals:
-    x = np.linspace(-1, 1, num=interval)
+nodes = [2, 3, 4, 5, 10, 20, 50, 100]
+for node in nodes:
+    x = np.linspace(-1, 1, node)
     y = func(x)
-    solution = integrate.simps(y, x)
-    print(solution)
+    print(f"Number of nodes: {node}")
+    print(f"Gauss-Legendre : {integrate.quadrature(func, -1, 1, maxiter=node)}")
+    print(f"Trapezoidal Rule: {integrate.trapz(y, x)}")
+    print(f"Simpson's Rule : {integrate.simps(y, x)}")
+    print()
 
-print()
-for i in range(0, 100):
-    print(str(i) + ": " + str(integrate.quadrature(func, -1, 1, maxiter=i)))
